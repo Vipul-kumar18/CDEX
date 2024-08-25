d903a1a035125f71350be99dec486632772fcfe5
Vipul Rohit <vipulrohit127@gmail.com>
2024-08-25 15:35:07 +0530

    second

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,9 @@ import NextAuth from "next-auth"
 const handler = NextAuth(authConfig)
 
 export { handler as GET, handler as POST }
-
+/*
 console.log({
     clientId: process.env.GOOGLE_CLIENT_ID ?? "9 not found",
     clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "10 not found"
 })
+*/
